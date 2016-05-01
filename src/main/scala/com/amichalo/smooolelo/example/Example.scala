fb757c29ddfc5fc78d97feee759b47176501f854
amichalo <andrzej.m.michalowski@gmail.com>
2016-05-01 18:34:35 +0200

    Add comments to the example

--- src/main/scala/com/amichalo/smooolelo/example/Example.scala
+++ src/main/scala/com/amichalo/smooolelo/example/Example.scala
@@ -15,16 +15,23 @@ object Example extends App {
 
   val config = ConfigFactory.load()
 
+  // Service config contains all Metadata about the service like id, type, group, env
   val serviceConfig = MoooleloServiceConfig.fromConfig(config.getConfig("mooolelo"))
+
+  // Client config contains Mooolelo endpoint, refresh intervals
   val clientConfig = MoooleloClientConfig.fromConfig(config.getConfig("mooolelo"))
 
+  // DataProvider provides data about our service during the runtime
   val dataProvider = new SMoooleloDataProvider(
+    // Yes... our service is always healthy...
     healthProvider = new HealthProvider { override def apply(): HealthStatus = HealthStatus(true, None) },
+    // We would like yo provide ip associated with wlan0 interface, it will also give you a hostname
     interfaceProvider = new BasicInterfaceProvider("wlan0")
   )
 
+  // Create a client ActorSystem, ActorMaterializer, ExecutionContext are being passed implicitly
   val client = new MoooleloClient(clientConfig, serviceConfig, dataProvider)
 
+  // When you are ready start the client, it will send first registration requests and schedule others
   client.start()
-
 }
